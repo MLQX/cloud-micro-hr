4c1d963c643b7bd62ffa99f143a969995cd9cc6f
ruoan <549918019@qq.com>
2020-04-01 08:14:30 +0800

    成功跑通web层

---
+++ microhrserver/microhr-web/src/main/java/org/example/microhr/HRApp.java
@@ -0,0 +1,12 @@
+package org.example.microhr;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HRApp.class, args);
+    }
+}
